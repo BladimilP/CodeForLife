e319cfaa9f23f33d8532ebda786e79d6aadacaa9
DeathlyProd <bladimil.paulino2257@gmail.com>
2021-07-22 17:46:21 -0400

    First commit

---
+++ marvelcharacters.js
@@ -0,0 +1,10 @@
+class Heros {
+    constructor(heroName, power, weakness){
+        this.heroName = heroName;
+        this.power = power;
+        this.weakness = weakness;
+    }
+  }
+  
+  const Batman = new Character("Batman", "Gagets", "Superpowers"),
+  const WonderWoman = new Character ("WonderWoman", "Superhuman_Abilities", "Poison")
